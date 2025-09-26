f7007c20ab27e37825e0be2b297ce68465981050
Kittipak <AliveKittipak@gmail.com>
2025-09-26 18:02:12 +0700

    init

---
+++ eazystore-ui/src/components/PageHeading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PageTitle from "./PageTitle";
+
+function PageHeading() {
+  return (
+    <div>
+      <PageTitle />
+    </div>
+  );
+}
+
+export default PageHeading;
