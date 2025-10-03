fc383399dcf197497f5c0258d7b236638194c8b7
Kittipak <AliveKittipak@gmail.com>
2025-10-03 17:20:57 +0700

    feat: section 8

--- eazystore-ui/src/components/PageHeading.jsx
+++ eazystore-ui/src/components/PageHeading.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import PageTitle from "./PageTitle";
 
-function PageHeading() {
+export default function PageHeading({ title, children }) {
   return (
-    <div>
-      <PageTitle />
+    <div className="text-center max-w-[576px] mx-auto px-4 py-6">
+      <PageTitle title={title} />
+      <p className="font-primary leading-6 text-gray-600">{children}</p>
     </div>
   );
 }
-
-export default PageHeading;
